a4fa0f0d7bc821f1802e91a944218be31d083c7e
generative-exploration <59022519+generative-exploration@users.noreply.github.com>
2020-01-06 16:55:31 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,61 +4,56 @@ var size = 200;
 let c;
 let myFont;
 
-function preload(){ 
+function preload() {
   img = loadImage("07.jpg");
   myFont = loadFont('FiraCode-Regular.ttf');
 }
 
 function setup() {
-  let c =  createCanvas(960, 1200);
+  let c = createCanvas(960, 1200);
   image(img, 0, 0);
   textFont(myFont);
   textSize(14);
 }
 
 function draw() {
-  copy(img, 0, height-20, width, height, 0, height-20, width, height);  
-   
+  copy(img, 0, height - 20, width, height, 0, height - 20, width, height);
+
   if (mouseIsPressed) {
-    //copy und .get haben leicht andere funktionen als in processing
-   // let d = get(mouseX+distance, mouseY+distance, lengthX, lengthY);
-   // image(d, mouseX, mouseY);
-    copy(mouseX, mouseY, size, size, mouseX+distance, mouseY+distance, size, size);
-  
+    copy(mouseX, mouseY, size, size, mouseX + distance, mouseY + distance, size, size);
   }
-  
+
   fill(255);
   textAlign(CENTER);
-  text("size: "+ size +"  distance: "+distance, width/2, height-5);
+  text("size: " + size + "  distance: " + distance, width / 2, height - 5);
 }
 
-function keyPressed() { 
-   if (key === 's') {
-      saveCanvas(c, 'gen_exploration', 'jpg');
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas(c, 'gen_exploration', 'jpg');
   }
-  
+
   if (key == 'r') {
-    image (img, 0, 0);
+    image(img, 0, 0);
   }
-  
 
-    if (keyCode == UP_ARROW) {
-      size = size+20;
+
+  if (keyCode == UP_ARROW) {
+    size = size + 20;
+  }
+  if (keyCode == DOWN_ARROW) {
+    size = size - 20;
+    if (size == 40) {
+      size = size + 20;
     }
-    if (keyCode == DOWN_ARROW) {
-      size = size-20;
-      if ( size == 40) {
-        size = size+20;
-      }
+  }
+  if (keyCode == RIGHT_ARROW) {
+    distance = distance + 1;
+  }
+  if (keyCode == LEFT_ARROW) {
+    distance = distance - 1;
+    if (distance == 0) {
+      distance = distance + 1;
     }
-    if (keyCode == RIGHT_ARROW) {
-      distance = distance+1;
-    }
-    if (keyCode == LEFT_ARROW){
-      distance = distance-1;
-     if ( distance == 0) {
-      distance = distance+1;
-      }
-    }
+  }
 }
-
